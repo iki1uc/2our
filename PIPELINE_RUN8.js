ae4862537b42fe9d074ff84869c9af87ad4e7bde
iki1uc <entertrainer@web.de>
2026-08-20 20:52:39 +0200

    Update PIPELINE_RUN8.js

--- PIPELINE_RUN8.js
+++ PIPELINE_RUN8.js
@@ -1,5 +1,3 @@
-// PIPELINE_RUN8.js – 4‑Stufen‑Pipeline
-
 import { VEC_CORE } from "./VEC_CORE.js";
 import { FLX_CORE } from "./FLX_CORE.js";
 import { FORM_ENGINE } from "./FORM_ENGINE.js";
@@ -7,32 +5,29 @@ import { FORM_MATRIX } from "./FORM_MATRIX.js";
 import { SYN_CORE } from "./syn.js";
 
 export function PIPELINE_RUN8(input) {
-    const { ion, echo, mia, formCore, t = 0 } = input || {};
+
+    const { ion, echo, mia, formCore, t = 0 } = input;
 
     // Stage 0 – VEC
     const vec = VEC_CORE({
-        pulse: ion?.pulse ?? mia?.pulse ?? 0,
-        warp: ion?.warp ?? echo?.warp ?? 0,
-        charge: ion?.charge ?? mia?.charge ?? 0
+        pulse: ion.pulse,
+        warp: ion.warp,
+        charge: ion.charge
     });
 
     // Stage 1 – FLX
     const flx = FLX_CORE({
         amplitude: vec.x,
-        resonance: echo?.resonance ?? 0,
+        resonance: echo.resonance,
         origin: vec.z
     });
 
     // Stage 2 – FORM
-    const matrix = FORM_MATRIX(formCore || []);
+    const matrix = FORM_MATRIX(formCore);
     const form = FORM_ENGINE(matrix, t);
 
     // Stage 8 – SYN
-    const syn = SYN_CORE(
-        mia || { fusion: 50 },
-        echo || { state: 'loud', resonance: 30 },
-        form
-    );
+    const syn = SYN_CORE(mia, echo, form);
 
     return {
         stage0: vec,
